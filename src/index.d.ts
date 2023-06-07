0857f2ccb5e8f231ed54df7026d9d6dc098084e4
Josh Liben <josh@junipercds.com>
2023-06-07 12:34:31 -0400

    export as namespace

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,14 @@
+export as namespace FhirLib
 import Client from "fhirclient/lib/Client";
 
-export = SmartLaunchHandler
-
-declare namespace FhirLib {
-  export function epicLaunch(clientId: string, redirect: string, iss: string): Promise<string | void>
-}
-declare enum EMR {
+// declare module 'fhir-lib' {
+  
+export function epicLaunch(clientId: string, redirect: string, iss: string): Promise<string | void>
+export enum EMR {
   CERNER = 'cerner',
   EPIC = 'epic',
   SMART = 'smart',
   NONE = 'none'
 }
-declare const SmartLaunchHandler: (clientID: string, emrType: EMR) => Promise<Client> | undefined
+export const SmartLaunchHandler: (clientID: string, emrType: EMR) => Promise<Client> | undefined
+// }
