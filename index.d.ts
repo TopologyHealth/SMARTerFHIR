41bf2839bc1b4fdc0fa80044b0ed425fc89b0a5f
Josh Liben <josh@junipercds.com>
2023-06-07 13:21:53 -0400

    all files at root level

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,14 @@
-export as namespace FhirLib
 import Client from "fhirclient/lib/Client";
 
-// function epicLaunch(clientId: string, redirect: string, iss: string): Promise<string | void>
-export enum EMR {
-  CERNER = 'cerner',
-  EPIC = 'epic',
-  SMART = 'smart',
-  NONE = 'none'
+declare function SmartLaunchHandler(clientID: string, emrType: SmartLaunchHandler.EMR): Promise<Client> | undefined
+declare namespace SmartLaunchHandler{
+  export enum EMR {
+    CERNER = 'cerner',
+    EPIC = 'epic',
+    SMART = 'smart',
+    NONE = 'none'
+  }
+  export function epicLaunch(clientId: string, redirect: string, iss: string): Promise<string | void>
 }
-export default function SmartLaunchHandler(clientID: string, emrType: EMR): Promise<Client> | undefined
+
+export = SmartLaunchHandler;
--- index.js
+++ index.js
@@ -1,3 +1,58 @@
 'use strict'
 
-module.exports = require('./src/smartLaunch.js');
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+const FHIR = require("fhirclient");
+
+const EMR = {
+  CERNER: 'cerner',
+  EPIC: 'epic',
+  SMART: 'smart',
+  NONE: 'none',
+}
+
+async function epicLaunch(clientId, redirect, iss) {
+  const scope = "launch online_access openid fhirUser"
+  const redirect_uri = redirect ?? '';
+  return FHIR.oauth2.authorize({
+    client_id: clientId,
+    iss: iss,
+    "redirect_uri": redirect_uri,
+    scope: scope,
+  });
+}
+
+function SmartLaunchHandler(clientID, emrType) {
+  const curEMR = emrType;
+  const [fhirClient, setFhirClient] = useState(undefined);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // Authorize with the EHR
+        const queryString = window.location.search;
+        const originString = window.location.origin;
+        const urlParams = new URLSearchParams(queryString);
+        const iss = urlParams.get('iss');
+
+        if (iss !== null && iss.includes(emrType))
+          await epicLaunch(clientID, originString, iss);
+
+        setFhirClient(FHIR.oauth2.ready());
+      }
+      catch (e) {
+        if (e instanceof Error) {
+          throw e;
+        }
+      }
+    })();
+  }, [clientID, curEMR]);
+  return fhirClient
+}
+
+
+module.exports = SmartLaunchHandler;
