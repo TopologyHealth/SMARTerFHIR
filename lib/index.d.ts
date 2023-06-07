beea414885a2ef76dec6acb4bca073b6624aa1af
Josh Liben <josh@junipercds.com>
2023-06-07 14:16:12 -0400

    adding lib for private gitrepo use

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+import Client from "fhirclient/lib/Client";
+declare enum EMR {
+    CERNER = "cerner",
+    EPIC = "epic",
+    SMART = "smart",
+    NONE = "none"
+}
+export declare function SmartLaunchHandler(clientID: string, emrType: EMR): Promise<Client> | undefined;
+export {};
