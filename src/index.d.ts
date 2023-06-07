216f970730dbd5b992aa2207453320459683a8ff
Josh Liben <josh@junipercds.com>
2023-06-07 12:43:18 -0400

    import smartlauncher from js file in indexDTS

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,12 @@
 export as namespace FhirLib
 import Client from "fhirclient/lib/Client";
-
-// declare module 'fhir-lib' {
+import SmartLaunchHandler  from "./smartLaunch";
   
-export function epicLaunch(clientId: string, redirect: string, iss: string): Promise<string | void>
-export enum EMR {
+function epicLaunch(clientId: string, redirect: string, iss: string): Promise<string | void>
+enum EMR {
   CERNER = 'cerner',
   EPIC = 'epic',
   SMART = 'smart',
   NONE = 'none'
 }
 export const SmartLaunchHandler: (clientID: string, emrType: EMR) => Promise<Client> | undefined
-// }
