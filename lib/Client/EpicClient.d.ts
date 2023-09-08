eac02e57a8de64cc5772cae7da19b057912acdbc
Josh Liben <josh@topology.health>
2023-09-08 16:46:29 -0400

    Add necesary endpoints for Cerner Standalone Launch

--- lib/Client/EpicClient.d.ts
+++ lib/Client/EpicClient.d.ts
@@ -5,6 +5,7 @@ import BaseClient, { EMR_ENDPOINTS } from "./BaseClient";
  Represents the EpicClient class, extending the BaseClient.
  */
 export default class EpicClient extends BaseClient {
+    static readonly AUTHORIZE_ENDPOINT = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize";
     static readonly TOKEN_ENDPOINT = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token";
     static readonly R4_ENDPOINT = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/";
     static getEndpoints(): EMR_ENDPOINTS;
